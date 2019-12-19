764ec5d0795e46765a00bfb5902c82a8d1db9862
Markus L <markus@paradize.org>
2019-12-19 10:33:40 +0800

    Minor fix to the Temperature and Humidity driver

--- drivers/expanded/tasmota-generic-th-device-expanded.groovy
+++ drivers/expanded/tasmota-generic-th-device-expanded.groovy
@@ -754,4 +754,24 @@ def configuration_model_tasmota()
 }
 
 /* Helper functions included in all drivers with Temperature and Humidity */
+private getAdjustedTemp(value) {
+    value = Math.round((value as Double) * 100) / 100
 
+	if (tempOffset) {
+	   return value =  value + Math.round(tempOffset * 100) /100
+	} else {
+       return value
+    }
+    
+}
+
+private getAdjustedHumidity(value) {
+    value = Math.round((value as Double) * 100) / 100
+
+	if (humidityOffset) {
+	   return value =  value + Math.round(humidityOffset * 100) /100
+	} else {
+       return value
+    }
+    
+}
